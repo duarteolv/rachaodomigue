92885cc56e9ac6874ffb20d230c586395a68dc25
Felipe <fm3209742@gmail.com>
2023-03-28 19:13:14 -0300

    add email functionality

---
+++ assets/js/app.js
@@ -0,0 +1,42 @@
+(function () {
+  const CLOSE_BUTTON_MESSAGE = "Fechar";
+  window.onload = function () {
+    const form = document.querySelector("form.send-mail");
+
+    form.addEventListener("submit", async function (e) {
+      e.preventDefault();
+      const validator = new Pristine(this);
+
+      if (validator.validate()) {
+        Swal.showLoading();
+        const userData = validator.fields.reduce((acc, input) => {
+          acc[input.input.name] = input.input.value;
+          return acc;
+        }, {});
+        const isSuccess = await fetch("/contato", {
+          method: "POST",
+          body: JSON.stringify(userData),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }).then((r) => r.json());
+        Swal.hideLoading();
+        if (isSuccess.success) {
+          return Swal.fire({
+            title: "Gooooool!",
+            text: "Sua mensagem foi enviada!",
+            icon: "success",
+            confirmButtonText: CLOSE_BUTTON_MESSAGE,
+          });
+        } else {
+          return Swal.fire({
+            title: "Na trave !",
+            text: "Verifique seus dados ou tente novamente mais tarde!",
+            icon: "error",
+            confirmButtonText: CLOSE_BUTTON_MESSAGE,
+          });
+        }
+      }
+    });
+  };
+})();
